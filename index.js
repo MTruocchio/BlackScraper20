3aae18d91cecfb1259840721e44bccec94613379
Marcelino <santanatruocchio@gmail.com>
2020-11-22 22:29:10 -0300

    commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({headless: false});
+
+const url = "https://www.kabum.com.br/cgi-local/site/produtos/descricao_ofertas.cgi?codigo=99683"
+const selector = "span.preco_desconto_avista-cm"
+const title = "Ryzen 5 3600"
+const site = "Kabum"
+
+const robo = async () => {
+const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto(url);
+
+  const resultado = await page.evaluate(() => {
+    return {
+        valor: document.querySelector(`span.preco_desconto_avista-cm`).innerHTML      
+    };
+  });
+
+  console.log(`o valor de ${title} em ${site} é:  ${resultado.valor}`)
 
   await browser.close();
-})();
+
+}  
+robo()
